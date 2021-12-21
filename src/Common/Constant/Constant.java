a3916758296c1edddc1a94d6bdc0692a550e4a39
ankhuong17 <SATT21.05.06@logigear.com>
2021-12-21 15:30:56 +0700

    Commit to git

--- src/Common/Constant/Constant.java
+++ src/Common/Constant/Constant.java
@@ -7,5 +7,5 @@ public class Constant {
     public static  final String RAILWAY_URL = "http://192.168.171.127:8082";
     public static  final String USERNAME = "khuong.nguyendiepan@gmail.com";
     public static  final String PASSWORD = "ankhuong3107";
-
+//s
 }
